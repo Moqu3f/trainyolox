328a37aefec57b4f1d609e31bdc8deba0759022d
Moqu3f <moqu3f@gmail.com>
2022-06-30 09:08:26 +0300

    Initial commit

---
+++ yolox/data/datasets/voc_classes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+VOC_CLASSES = (
+    "money-classification",
+    "1000uah",
+    "100uah",
+    "200uah",
+    "20uah",
+    "500uah",
+    "50uah"
+)
